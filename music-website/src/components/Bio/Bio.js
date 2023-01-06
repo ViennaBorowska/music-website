a59d850058f6f29d24bbe00e7d6f29f4442f5347
Vienna B <viennabdev@gmail.com>
2023-01-06 22:25:39 +0000

    adding imaged to bio

--- music-website/src/components/Bio/Bio.js
+++ music-website/src/components/Bio/Bio.js
@@ -1,24 +1,33 @@
 import React from "react";
 import "./Bio.css";
+import gbImg from "./GB-bw.png";
+
 export default function Bio() {
   return (
     <section>
-      <p className="about-me">
-        Hi there, I'm a junior fullstack developer based in Manchester, NW U.K.{" "}
-        <br></br>
-        <br></br>I am a fast learner in posession of a passion to do good, and a
-        keen problem-solving mind. I like to approach problems like a detective
-        solving a mystery, working ceaselessly and methodically until a solution
-        is reached.
-        <br></br>
-        <br></br>I am highly organised with critical attention to detail, so I
-        have found my skills well suited to back-end development - but I also
-        have a strong creative flair and enjoy building attractive, accessible,
-        responsive designs.
-      </p>
-      {/* <div className="logo-container">
+      <div className="bio-container">
+        <div className="will-fadeIn">
+          <img src={gbImg} alt="b&w of George Borowski"></img>
+        </div>
+        <div className="will-fadeIn">
+          <p className="bio-txt">
+            Hi there, I'm a junior fullstack developer based in Manchester, NW
+            U.K. <br></br>
+            <br></br>I am a fast learner in posession of a passion to do good,
+            and a keen problem-solving mind. I like to approach problems like a
+            detective solving a mystery, working ceaselessly and methodically
+            until a solution is reached.
+            <br></br>
+            <br></br>I am highly organised with critical attention to detail, so
+            I have found my skills well suited to back-end development - but I
+            also have a strong creative flair and enjoy building attractive,
+            accessible, responsive designs.
+          </p>
+        </div>
+        {/* <div className="logo-container">
         <div className="gb-logo"></div>
       </div> */}
+      </div>
     </section>
   );
 }
