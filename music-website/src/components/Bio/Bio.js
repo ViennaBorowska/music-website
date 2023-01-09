373463687860f8f069faf776e9c80af06314c435
Vienna B <viennabdev@gmail.com>
2023-01-09 21:49:28 +0000

    text placement and styling in bio.

--- music-website/src/components/Bio/Bio.js
+++ music-website/src/components/Bio/Bio.js
@@ -14,17 +14,17 @@ export default function Bio() {
         </div>
         <div className="will-fadeIn">
           <p className="bio-txt-2">
-            "As a first step towards an appreciation of George Borowski's
-            contribution to music, consider the following - <br></br>George’s
-            consistent motto is one that undoubtedly connects his epic music
-            with the music of others." <br></br>-
+            "As a first step towards an appreciation of <br></br> George
+            Borowski's contribution to music, consider the following - <br></br>
+            George’s consistent motto is one that undoubtedly <br></br> connects
+            his epic music with the music of others." <br></br>-
             <i>Jim Sutherland, BBC Manchester Introducing</i>
           </p>
         </div>
         <div className="will-fadeIn">
           <p className="bio-txt-3">
-            "I have never seen a rock'n'roll performer so completely connected{" "}
-            <br></br> with what he was doing onstage." <br></br> -
+            "I have never seen a rock'n'roll <br></br>performer so completely
+            connected <br></br> with what he was doing onstage." <br></br> -
             <i>Frank Black, Pixies</i>
           </p>
         </div>
