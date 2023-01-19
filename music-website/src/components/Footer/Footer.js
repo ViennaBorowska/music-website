bf330fc6bf4c6481eaad115aab5db7d2fc95bafe
Vienna B <viennabdev@gmail.com>
2023-01-19 21:11:49 +0000

    footer styling and responsive check

--- music-website/src/components/Footer/Footer.js
+++ music-website/src/components/Footer/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faLinkedin,
   faInstagram,
   faFacebook,
   faTwitter,
@@ -15,54 +14,52 @@ export default function Footer() {
   return (
     <footer>
       <div className="footer-container">
-        <div className="social-media-contact">
-          <div className="footer-link-holder">
-            <a
-              className="footer-links fa-2x"
-              href="https://www.instagram.com/georgeborowskiofficial/?hl=en"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
-            <a
-              className="footer-links fa-2x"
-              href="https://open.spotify.com/artist/5dWPhkuy8t8HQLRtlwOseb"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faSpotify} />
-            </a>
-            <a
-              className="footer-links fa-2x"
-              href="mailto:thecentrum@aol.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faEnvelope} />
-            </a>
-            <a
-              className="footer-links fa-2x"
-              href="https://www.facebook.com/GeorgeBorowskiOfficial/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
-            <a
-              className="footer-links fa-2x"
-              href="https://twitter.com/georgeborowski?lang=en"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-          </div>
-          <FontAwesomeIcon icon="fa-light fa-copyright" />
-          <p className="rights">
-            {year} &copy; George Borowski - All Rights Reserved
-          </p>
+        <div className="footer-link-holder">
+          <a
+            className="footer-links fa-2x"
+            href="https://www.instagram.com/georgeborowskiofficial/?hl=en"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a
+            className="footer-links fa-2x"
+            href="https://open.spotify.com/artist/5dWPhkuy8t8HQLRtlwOseb"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faSpotify} />
+          </a>
+          <a
+            className="footer-links fa-2x"
+            href="mailto:thecentrum@aol.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+          <a
+            className="footer-links fa-2x"
+            href="https://www.facebook.com/GeorgeBorowskiOfficial/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a
+            className="footer-links fa-2x"
+            href="https://twitter.com/georgeborowski?lang=en"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
         </div>
+        <FontAwesomeIcon icon="fa-light fa-copyright" />
+        <p className="rights">
+          {year} &copy; George Borowski - All Rights Reserved
+        </p>
       </div>
     </footer>
   );
