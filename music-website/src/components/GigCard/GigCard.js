70951f35387abd0d381b9b67460257a88cddda45
Vienna B <viennabdev@gmail.com>
2023-01-24 17:28:11 +0000

    moved gigs to json file and separated gig card out as separate component

---
+++ music-website/src/components/GigCard/GigCard.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import InfoIcon from "@mui/icons-material/Info";
+import LocalActivityIcon from "@mui/icons-material/LocalActivity";
+import Card from "@mui/material/Card";
+import { CardActionArea } from "@mui/material";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+
+export default function GigCard(props) {
+  return (
+    <ListItem
+      className="gig-list-item"
+      secondaryAction={
+        props.ticket ? (
+          <Tooltip title="Get Tickets">
+            <IconButton
+              className="ticket-button"
+              edge="end"
+              aria-label="tickets"
+              href={props.ticket}
+              target="_blank"
+            >
+              <LocalActivityIcon />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Tooltip title="Gig Info">
+            <IconButton
+              className="ticket-button"
+              edge="end"
+              aria-label="tickets"
+              href={props.link}
+              target="_blank"
+            >
+              <InfoIcon />
+            </IconButton>
+          </Tooltip>
+        )
+      }
+    >
+      <ListItemAvatar>
+        <Avatar alt="gig image" src={props.image} />
+      </ListItemAvatar>
+      <ListItemText
+        disableTypography
+        primary={
+          <Typography type="body2" style={{ color: "#8c52ff" }}>
+            `{props.date} | {props.title} | {props.time}`
+          </Typography>
+        }
+        secondary={
+          <Typography type="body2" style={{ color: "#FFFFFF" }}>
+            @ {props.place}
+          </Typography>
+        }
+      />
+    </ListItem>
+  );
+}
--- music-website/src/components/Live/Live.js
+++ music-website/src/components/Live/Live.js
@@ -1,24 +1,14 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "./Live.css";
-import AudiotrackIcon from "@mui/icons-material/Audiotrack";
-import Avatar from "@mui/material/Avatar";
 import Grid from "@mui/material/Grid";
-import IconButton from "@mui/material/IconButton";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
-import InfoIcon from "@mui/icons-material/Info";
-import LocalActivityIcon from "@mui/icons-material/LocalActivity";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import ListItemText from "@mui/material/ListItemText";
-import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
 import { Container } from "@mui/system";
-import QueensCraftPoster from "./posters/gb-poster-crafthouse.jpg";
-import HunchPoster from "./posters/gb-hunch-poster.jpg";
-import TheHive from "./posters/the-hive.jpg";
+import GigCard from "../GigCard/GigCard";
+import GigData from "../../data/gig-list.json";
 // function generate(element: React.ReactElement) {
 //   return [0, 1, 2].map((value) =>
 //     React.cloneElement(element, {
@@ -27,24 +17,9 @@ import TheHive from "./posters/the-hive.jpg";
 //   );
 // }
 
-const images = [
-  {
-    thumbnail: {
-      uri: QueensCraftPoster,
-      href: "https://www.facebook.com/theQueenDonny",
-    },
-  },
-  {
-    thumbnail: {
-      uri: HunchPoster,
-      href: "https://castindoncaster.com/whats-on/the-hunch-george-borowski/?fbclid=IwAR1YNFEku7CRSUBJc57X5lw72TWSqq0s_h3xB6a09ouyv6D6zjlvIsBMfd8",
-    },
-  },
-];
-
 export default function Live() {
   const [dense, setDense] = React.useState(false);
-
+  const [gigList, setSelectedCard] = useState(GigData);
   return (
     <section className="live-container">
       <Container>
@@ -60,10 +35,10 @@ export default function Live() {
             gridAutoColumns: "minmax(160px, 1fr)",
           }}
         >
-          {images.map((image) => (
-            <a href={image.thumbnail.href} target="_blank">
+          {gigList.map((poster) => (
+            <a href={poster.link} target="_blank">
               <ImageListItem>
-                <img src={image.thumbnail.uri} />
+                <img src={poster.poster} />
               </ImageListItem>
             </a>
           ))}
@@ -72,107 +47,17 @@ export default function Live() {
       <Grid item xs={12} md={6}>
         <div>
           <List dense={dense} className="gig-list-container">
-            <ListItem
-              className="gig-list-item"
-              secondaryAction={
-                <Tooltip title="Get Tickets">
-                  <IconButton
-                    className="ticket-button"
-                    edge="end"
-                    aria-label="tickets"
-                    href="https://castindoncaster.com/whats-on/the-hunch-george-borowski/"
-                    target="_blank"
-                  >
-                    <LocalActivityIcon />
-                  </IconButton>
-                </Tooltip>
-              }
-            >
-              <ListItemAvatar>
-                <Avatar alt="Cast Doncaster" src={HunchPoster} />
-              </ListItemAvatar>
-              <ListItemText
-                disableTypography
-                primary={
-                  <Typography type="body2" style={{ color: "#8c52ff" }}>
-                    WEDS 25/1: The Hunch + George Borowski
-                  </Typography>
-                }
-                secondary={
-                  <Typography type="body2" style={{ color: "#FFFFFF" }}>
-                    @ Cask, Doncaster
-                  </Typography>
-                }
-              />
-            </ListItem>
-            <ListItem
-              className="gig-list-item"
-              secondaryAction={
-                <Tooltip title="Gig Info">
-                  <IconButton
-                    edge="end"
-                    aria-label="gig-ticket"
-                    className="ticket-button"
-                  >
-                    <InfoIcon
-                      href="https://www.facebook.com/theQueenDonny"
-                      target="_blank"
-                    />
-                  </IconButton>
-                </Tooltip>
-              }
-            >
-              <ListItemAvatar>
-                <Avatar alt="Queen Crafthouse" src={QueensCraftPoster} />
-              </ListItemAvatar>
-              <ListItemText
-                disableTypography
-                primary={
-                  <Typography type="body2" style={{ color: "#8c52ff" }}>
-                    FRI 27/1: George Solo
-                  </Typography>
-                }
-                secondary={
-                  <Typography type="body2" style={{ color: "#FFFFFF" }}>
-                    @ The Queen Crafthouse & Kitchen, Doncaster
-                  </Typography>
-                }
-              />
-            </ListItem>
-            <ListItem
-              className="gig-list-item"
-              secondaryAction={
-                <Tooltip title="Gig Info">
-                  <IconButton
-                    edge="end"
-                    aria-label="gig-ticket"
-                    className="ticket-button"
-                  >
-                    <InfoIcon
-                      href="https://www.thehivelive.org/all-events"
-                      target="_blank"
-                    />
-                  </IconButton>
-                </Tooltip>
-              }
-            >
-              <ListItemAvatar>
-                <Avatar alt="The Hive" src={TheHive} />
-              </ListItemAvatar>
-              <ListItemText
-                disableTypography
-                primary={
-                  <Typography type="body2" style={{ color: "#8c52ff" }}>
-                    SUN 5/2: George & The Fabulous Wonderfuls
-                  </Typography>
-                }
-                secondary={
-                  <Typography type="body2" style={{ color: "#FFFFFF" }}>
-                    @ The Hive, Winsford for Independent Venue Week
-                  </Typography>
-                }
-              />
-            </ListItem>
+            {gigList.map((gig) => {
+              <GigCard
+                date={gig.date}
+                ticket={gig.ticket}
+                link={gig.link}
+                place={gig.place}
+                time={gig.time}
+                image={gig.image}
+                title={gig.title}
+              />;
+            })}
           </List>
         </div>
       </Grid>
